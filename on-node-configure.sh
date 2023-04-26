a040be4ae42779c11b3aaa947216d8757090bc56
dlucas-exelixis <126510966+dlucas-exelixis@users.noreply.github.com>
2023-04-26 13:49:46 -0400

    fix sources

--- on-node-configure.sh
+++ on-node-configure.sh
@@ -54,6 +54,8 @@ echo 'source /opt/ccp4/ccp4-8.0/bin/ccp4.setup-sh' >> /home/slurm/.profile
 echo 'source /opt/GPhL/ALL_snapshot_20230222/setup.sh' >> /home/slurm/.profile
 echo 'export PATH="/opt/xds/XDS-INTEL64_Linux_x86_64:$PATH"' >> /home/slurm/.profile
 echo 'export BDG_TOOL_MOGUL=/opt/csds/CSD_2022/bin/mogul' >> /home/slurm/.profile
+echo 'source /opt/hkl/HKL2000_v722-Linux-x86_64/hkl_setup.sh' >> /home/slurm/.profile
+echo 'source /usr/local/phenix-1.20.1-4487/phenix_env.sh' >> /home/slurm/.profile
 echo "/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID" >> /home/slurm/.profile
 echo "/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID" >> /home/ubuntu/.profile
 echo "Completed setup of Global Phasing Software!"
