a3cca8bf6fbb703af00cbf8d034f7ec44bf6d6ee
dlucas-exelixis <126510966+dlucas-exelixis@users.noreply.github.com>
2023-04-26 13:24:07 -0400

    fix global phasing

--- on-node-configure.sh
+++ on-node-configure.sh
@@ -48,13 +48,23 @@ CRYOSPARC_VERSION=${8:-latest}
 # Install the AWS CLI
 pip3 install --upgrade awscli boto3
 
+# Customize for Global Phasing
+echo "Setting up Global Phasing Software..."
+echo 'source /opt/ccp4/ccp4-8.0/bin/ccp4.setup-sh' >> /home/slurm/.profile
+echo 'source /opt/GPhL/ALL_snapshot_20230222/setup.sh' >> /home/slurm/.profile
+echo 'export PATH="/opt/xds/XDS-INTEL64_Linux_x86_64:$PATH"' >> /home/slurm/.profile
+echo 'export BDG_TOOL_MOGUL=/opt/csds/CSD_2022/bin/mogul' >> /home/slurm/.profile
+echo "/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID" >> /home/slurm/.profile
+echo "/opt/csds/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID" >> /home/home/.profile
+echo "Completed setup of Global Phasing Software!"
+
 # Activate Mogul
-echo "Checking if we need to activate mogul.."
-if [[ -d $CSDS_DIR ]]; then
-  echo "Activating mogul..."
-  $CSDS_DIR/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID
-fi
-echo "Activation complete!"
+#echo "Checking if we need to activate mogul.."
+#if [[ -d $CSDS_DIR ]]; then
+#  echo "Activating mogul..."
+#  $CSDS_DIR/CSD_2022/bin/ccdc_activator -a -k $MOGUL_ACTIVATION_ID
+#fi
+#echo "Activation complete!"
 
 # Configure AWS
 AWS_DEFAULT_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | rev | cut -c 2- | rev)
